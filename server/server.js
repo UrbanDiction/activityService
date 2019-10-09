062e906c3d08151574dac53dc9d914c519364ca0
NickGatti <nick.gatti@gmail.com>
2019-10-08 18:50:06 -0700

    added basic scaffolding except jest and enzyme

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/../public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// require( '../config/routes.js' )( app )
+
+app.listen(port, function () {
+    console.log('Listening on', port);
+});
+
