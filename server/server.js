4141de33958d16f4b03ab04a073e63fba803335e
NickGatti <nick.gatti@gmail.com>
2019-10-09 10:10:47 -0700

    airbnb working

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
+
+const bodyParser = require("body-parser");
+
 const app = express();
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + '/../public'))
+app.use(express.static(`${__dirname}/../public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require( '../config/routes.js' )( app )
+require("../config/routes.js")(app);
 
-app.listen(port, function () {
-    console.log('Listening on', port);
+app.listen(port, () => {
+  console.log("Listening on", port);
 });
-
