de00b9801c89938029b763a1b686aec0417fd5a0
NickGatti <nick.gatti@gmail.com>
2019-10-09 14:20:23 -0700

    process args

--- server/server.js
+++ server/server.js
@@ -2,15 +2,20 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const path = require("path")
+
 const app = express();
 const port = process.env.PORT || 8000;
+const processArgs = process.argv.slice(2)
 
-app.use(express.static(`${__dirname}/../public`));
+app.use(express.static(path.join(__dirname, '/../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./config/routes.js")(app);
 
 app.listen(port, () => {
-  console.log("Listening on", port);
+  if (processArgs.includes("development")) {
+    console.log("Listening on", port);
+  }
 });
