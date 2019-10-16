ca442faecb107e678bfb96a64010f0a9320c8c57
NickGatti <nick.gatti@gmail.com>
2019-10-16 14:02:43 -0700

    activity component made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
+// eslint-disable-next-line no-unused-vars
+import ActivityChart from "./components/activity/ActivityChart.jsx";
+
 const fetch = require("node-fetch");
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activity: null
+    };
+  }
+
   // eslint-disable-next-line class-methods-use-this
   componentDidMount() {
     fetch("/activity/word/getVisits", {
@@ -13,7 +23,6 @@ class App extends React.Component {
       }
     })
       .then(data => {
-        // console.log(data);
         return data.json();
       })
       .then(({ visitsQuery }) => {
@@ -26,80 +35,88 @@ class App extends React.Component {
         visits[0].unshift("x");
         visits[1].unshift("Activity");
 
-        console.log(visits);
+        this.setState({ activity: visits });
+        this.populateC3Grid();
+      });
+  }
 
-        const chart = document.getElementById("chart");
-        if (chart) {
+  // eslint-disable-next-line class-methods-use-this
+  populateC3Grid() {
+    const chart = document.getElementById("chart");
+    if (chart) {
+      // eslint-disable-next-line no-unused-vars
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      // eslint-disable-next-line no-undef
+      c3.generate({
+        size: {
+          height: 200
+        },
+        padding: {
+          left: 10,
+          right: 10
+        },
+        data: {
+          x: "x",
+          columns: this.state.activity,
+          type: "bar"
+        },
+        tooltip: {
           // eslint-disable-next-line no-unused-vars
-          const months = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-          ];
-          // eslint-disable-next-line no-undef
-          c3.generate({
-            size: {
-              height: 200
-            },
-            padding: {
-              left: 10,
-              right: 10
-            },
-            data: {
-              x: "x",
-              columns: visits,
-              type: "bar"
-            },
-            // tooltip: {
-            //   // eslint-disable-next-line no-unused-vars
-            //   contents(data, defaultTitleFormat, defaultValueFormat, color) {
-            //     return `<table class="c3-tooltip"><tr><td>${
-            //       months[data[0].x.getMonth()]
-            //     } ${data[0].x.getFullYear()}</td></tr></table>`;
-            //   }
-            // },
-            grid: {
-              x: {
-                show: false
-              }
-            },
-            axis: {
-              x: {
-                type: "timeseries",
-                tick: {
-                  count: 2,
-                  show: false,
-                  format: "%Y",
-                  outer: false
-                }
-              },
-              y: {
-                show: false
-              }
-            },
-            legend: {
-              show: false
-            },
-            bar: {
-              width: 1
+          contents(data, defaultTitleFormat, defaultValueFormat, color) {
+            return `<table class="c3-tooltip"><tr><td>${
+              months[data[0].x.getMonth()]
+            } ${data[0].x.getFullYear()}</td></tr></table>`;
+          }
+        },
+        grid: {
+          x: {
+            show: false
+          }
+        },
+        axis: {
+          x: {
+            type: "timeseries",
+            tick: {
+              count: 2,
+              show: false,
+              format: "%Y",
+              outer: false
             }
-          });
+          },
+          y: {
+            show: false
+          }
+        },
+        legend: {
+          show: false
+        },
+        bar: {
+          width: 1
         }
       });
+    }
   }
 
   // eslint-disable-next-line
   render() {
-    return <div>React loaded!!!</div>;
+    return (
+      <div>
+        <ActivityChart />
+      </div>
+    );
   }
 }
 
